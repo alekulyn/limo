502220e50d76e020bd87aca5446b04743b59c5e7
alekulyn <alekulyn@users.noreply.github.com>
2025-08-22 22:49:39 +0900

    Add a delete action/context menu

--- src/ui/downloadview.h
+++ src/ui/downloadview.h
@@ -3,15 +3,14 @@
 #include <QTreeView>
 #include <QMouseEvent>
 #include <QFileSystemModel>
-#include <QDebug>
+#include <QMessageBox>
+#include <QMenu>
 
 class DownloadView : public QTreeView
 {
   Q_OBJECT
 public:
-  explicit DownloadView(QWidget* parent) : QTreeView(parent)
-  {
-  }
+  explicit DownloadView(QWidget* parent) : QTreeView(parent) {}
 signals:
   /*!
    * \brief Signals files have been dropped into this widget.
@@ -24,9 +23,27 @@ protected:
     auto index = indexAt(event->pos());
     if (index.isValid()) {
       auto filepath = static_cast<QFileSystemModel *>(model())->filePath(index);
-      qDebug() << "Double clicked on file:" << filepath;
       emit modAdded(QList<QUrl>{QUrl::fromLocalFile(filepath)});
     }
     QTreeView::mouseDoubleClickEvent(event);
+  }
+
+  void contextMenuEvent(QContextMenuEvent* event) override {
+    QModelIndex index = indexAt(event->pos());
+    if (!index.isValid()) return;
+
+    QMenu menu(this);
+    QAction* deleteAction = menu.addAction("Delete");
+    connect(deleteAction, &QAction::triggered, this, [this, index]() {
+      QString filepath = static_cast<QFileSystemModel *>(model())->filePath(index);
+      QFile file(filepath);
+      if (file.remove()) {
+        // TODO: Emit a signal or update the model to reflect the deletion. Also show a disable-able confirmation message.
+      } else {
+        QMessageBox::warning(this, "Error", "Failed to delete the file.");
+      }
+    });
+
+    menu.exec(event->globalPos());
   }
 };
